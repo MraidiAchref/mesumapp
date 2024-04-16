2241a6ea8d9b40b5e2d5a1a0d5e286cdd8c07ad7
mraidi achref <mradiachref@gmail.com>
2024-04-16 23:11:34 +0100

    added the squalatant of 'a propos' page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Ajoutez from './pages/Ajoutez';
+import Apropos from './pages/Apropos'
 import Navbarr from './components/Navbarr';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Rejoindrefinal from './components/Rejoindrefinal';
 import './pages/Explorez';
 import Explorez from './pages/Explorez';
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +21,7 @@ function App() {
           <Route path="" element={<Home />} />
           <Route path="/ajout" element={<Ajoutez />} />
           <Route path="/Explorez" element={<Explorez />} />
+          <Route path="/apropos" element={<Apropos />} />
         </Routes>
       </BrowserRouter>
       <Rejoindrefinal className="fixed-top" />
---
+++ src/pages/Apropos.js
@@ -0,0 +1,10 @@
+import "./AproposStyle.css";
+export default function Apropos() {
+  return (
+    <div className="page-container">
+      <h1>presentation de l'equipe:  CANVA</h1>
+      <h1>map emplacement esprit</h1>
+      
+    </div>
+  );
+}
